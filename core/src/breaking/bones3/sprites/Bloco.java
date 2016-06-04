2240d8a3a327b83a5a3a0e95c0d798e40da3291c
Dario Woloszin <3h2so4@gmail.com>
2016-06-04 17:55:25 -0300

    vmo q vmo

---
+++ core/src/breaking/bones3/sprites/Bloco.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package breaking.bones3.sprites;
+
+/**
+ *
+ * @author L4R15544
+ */
+public class Bloco {
+    
+}
